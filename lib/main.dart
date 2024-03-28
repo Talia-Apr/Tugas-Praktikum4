2181f3fe8f51a74276f5fad87d59ef2dfce762fa
Talia-Apr <68282150+Talia-Apr@users.noreply.github.com>
2024-03-28 07:03:48 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,26 @@ import 'package:latihan/recipe.dart';
 import 'package:latihan/recipe_detail.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RecipeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({Key? key}) : super(key: key);
+  static const MaterialColor white = MaterialColor(
+    0xFFFFFFFF,
+    <int, Color>{
+      50: Color(0xFFFFFFFF),
+      100: Color(0xFFFFFFFF),
+      200: Color(0xFFFFFFFF),
+      300: Color(0xFFFFFFFF),
+      400: Color(0xFFFFFFFF),
+      500: Color(0xFFFFFFFF),
+      600: Color(0xFFFFFFFF),
+      700: Color(0xFFFFFFFF),
+      800: Color(0xFFFFFFFF),
+      900: Color(0xFFFFFFFF),
+    }
+  );
 
   // This widget is the root of your application.
   @override
